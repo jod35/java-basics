927abc9cacd358a934df81042e82502f7742651e
jod35 <jodestrevin@gmail.com>
2019-10-10 09:28:27 +0300

    Another look on types

---
+++ TypeDifferences.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.awt.*;
+
+public class TypeDifferences {
+    public static void main(String[] args) {
+        Point point1= new Point(1,1);
+        Point point2 =point1;
+
+        point1.x=2;
+
+        System.out.println(point2);
+    }
+}
---
+++ Types.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Date;
+
+public class Types {
+    public static void main(String[] args) {
+        byte age =30; //primitive (dont have members)
+        Date now =new Date(); //reference type (have members)
+
+        System.out.println(now);
+    }
+}
