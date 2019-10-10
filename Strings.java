6811b6d8f792359ce1268ec12f59be825c1dc2e7
jod35 <jodestrevin@gmail.com>
2019-10-10 10:29:56 +0300

    some methods

--- Strings.java
+++ Strings.java
@@ -1,4 +1,17 @@
 package com.company;
 
 public class Strings {
+    public static void main(String[] args) {
+//        String message = "Hello Stranger";
+//
+//        System.out.println(message.endsWith("Stranger")); // returns true
+
+        //string concatenation
+        String message = "jona";
+        String sentence ="My name is ";
+        System.out.println(sentence + message.replace("s","tt"));
+
+
+        // returns "My name is jona"
+    }
 }
