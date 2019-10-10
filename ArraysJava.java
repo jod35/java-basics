e14758dbe7e7c2cb40bad3e8f6199e99a80e67f6
jod35 <jodestrevin@gmail.com>
2019-10-10 10:50:16 +0300

    Array sorting

---
+++ ArraysJava.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Arrays {
+    public static void main(String[] args) {
+//        int []  numbers= new int [5];
+//        numbers[0]=1;
+//        numbers[1]=2;
+//        numbers[2]=3;
+//
+//        System.out.println(java.util.Arrays.toString(numbers)); // this method returns a string representatin of the array
+////        [1, 2, 3, 0, 0] makes the unmentioned items to 0 since its int
+
+//        but this is an ols way
+
+        //the new way
+
+        int [] numbers ={1,2,3,4,5};
+
+        System.out.println(numbers.length);
+        //returns 5
+        // arrays have a fixed length
+        
+
+
+
+    }
+}
