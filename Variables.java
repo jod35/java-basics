cf5e45ab32f685aa3a4055cc73473d3047d558be
jod35 <jodestrevin@gmail.com>
2019-10-10 08:58:01 +0300

    Variables and data types

---
+++ Variables.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 35;
+        int myage =age;
+        System.out.println(myage);
+    }
+
+}
